4c4aa5018bd3035d4a7d9921d95a08a6c885db9e
KatarzynaBukowska <49091303+KatarzynaBukowska@users.noreply.github.com>
2019-04-01 20:40:03 +0200

    Add files via upload

---
+++ assets/data.js
@@ -0,0 +1,51 @@
+const vinylRecords = [{
+    
+    id: 1,
+    artist: 'Lonker See',
+    title: 'One Eye Sees Red LP (black)',
+    price: 45,
+    description: 'On April 5th 2018 Instant Classic released a brand new album from Lonker See. Entitled “One Eye Sees Red”, the record consists of three complex tracks with a running time of over 40 minutes.',
+    cover: 'https://instant-classic.com/wp-content/uploads/2018/04/lonker_see_cover.jpg',
+},
+{
+    id: 2,
+    artist: 'Kristen',
+    title: 'Las LP (black)',
+    price: 45,
+    description: '“LAS” is the eighth studio album from long-running Polish alternative heroes Kristen. This is where their superb musicianship blends into something that can only be described as a modern take on Neu! and Cluster legacy with a hint of premier league songwriting.',
+    cover: 'https://instant-classic.com/wp-content/uploads/2017/12/kristen_las.jpg',
+},
+{
+    id: 3,
+    artist: 'Merkabah',
+    title: 'Million Miles 2LP (black)',
+    price: 70,
+    description: 'On November 10th 2017 Instant Classic will release a new album from Poland, Warsaw-based experimental group Merkabah. Entitled “Million Miles”, the record contains eight new tracks merging free jazz and avantgarde metal.',
+    cover: 'https://instant-classic.com/wp-content/uploads/2017/12/merkabah_million_miles.jpg',  
+},
+{
+    id: 4,
+    artist: 'Zimpel / Ziołek',
+    title: 'S/T LP (black)',
+    price: 45,
+    description: 'This is the debut album from the duo of Wacław Zimpel (Hera, Ircha) and Kuba Ziołek (Stara Rzeka, Innercity Ensemble, Alameda Organisation, Kapital). The record entitled simply “Zimpel/Ziołek” consists of four tracks that might surprise those familiar with musicians’ previous recordings.',
+    cover: 'https://instant-classic.com/wp-content/uploads/2017/12/zimpel_ziolek.jpg',
+},
+{
+    id: 5,
+    artist: 'Wild Books',
+    title: '2 LP (black)',
+    price: 45,
+    description: 'On May 29 2017 Instant Classic released “2” – a new album from Warsaw-based garage rocking trio Wild Books.',
+    cover: 'https://instant-classic.com/wp-content/uploads/2017/12/wild_books_2.jpg', 
+},
+{
+    id: 6,
+    artist: 'Jakub Ziołek / Innercity Ensemble',
+    title: 'One Louder at 5 7″',
+    price: 30,
+    description: '7″ EP released on the occasion of label’s 5th birthday party in 2016. Includes two cover versions: Jakub Ziołek (Stara Rzeka) performing Silver Apples’ “Misty Mountain” and Innercity Ensemble with Jaśmina Polak performing Faust’s “It’s a Rainy Day Sunshine Girl”. Comes in a kraft recycled sleeve.',
+    cover: 'https://instant-classic.com/wp-content/uploads/2017/12/one_louder_ep.jpg',
+}];
+
+export default vinylRecords;
